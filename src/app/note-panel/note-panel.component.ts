7969d1d22fa31311bed25cbd4858fc2d0cc7e31d
ngoller <Nicolas.Goller@utah.edu>
2020-10-04 21:25:18 -0700

    Added octave configuration, accidental configuration, and auto beaming.

--- src/app/note-panel/note-panel.component.ts
+++ src/app/note-panel/note-panel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+
 import Vex from "vexflow";
-import { Observable } from 'rxjs';
 import { MidiService } from '../midi.service';
 import { DisplayNote } from '../../display-note'
 
@@ -35,6 +35,10 @@ export class NotePanelComponent implements OnInit {
   clef: string =  'treble';
   speed: number = 1;
   nps: number = 1;
+  sharps: boolean = false;
+  flats: boolean = false;
+  minOctave: number = 4;
+  maxOctave: number = 4;
 
   constructor(private midi: MidiService) {
     this.boundUpdate = this.update.bind(this);
@@ -99,14 +103,19 @@ export class NotePanelComponent implements OnInit {
   }
 
   getRandomNote(): Vex.Flow.StaveNote {
-    const durations = ['8', '4', '2', '1'];
-    let letter = String.fromCharCode('a'.charCodeAt(0) + Math.floor(Math.random() * 7))
-    let octave = `${4 + Math.floor(Math.random() * 1)}`
-    let acc = '';
+    // const durations = ['8', '4', '2', '1'];
+    const letter = String.fromCharCode('a'.charCodeAt(0) + Math.floor(Math.random() * 7))
+    const diff =  this.maxOctave - this.minOctave + 1;
+    const octave = `${this.minOctave + Math.floor(Math.random() * diff)}`
+    const accidentals = [''];
+    if (this.flats) { accidentals.push('b'); }
+    if (this.sharps) { accidentals.push('#'); }
+    const acc = accidentals[Math.floor(Math.random()*accidentals.length)];
     const note = new Vex.Flow.StaveNote({
       clef: this.clef,
       keys: [`${letter}${acc}/${octave}`],
       duration: '4',
+      auto_stem: true,
     })
    
     if (acc) note.addAccidental(0, new Vex.Flow.Accidental(acc));
@@ -117,7 +126,6 @@ export class NotePanelComponent implements OnInit {
   drawNote(c: Vex.IRenderContext): void {
     const group = c.openGroup(); // create an SVG group element
     const note = this.getRandomNote();
-
     const voice = new Vex.Flow.Voice({num_beats: 1,  beat_value: 4});
     voice.addTickables([note]);
     const formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 2000);
